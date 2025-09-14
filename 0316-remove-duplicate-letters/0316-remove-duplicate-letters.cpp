b932b3d95e2fe4b16e0c8593d9825ba9ca720b3e
Abhishek singh <abhisheksinghmd2202@gmail.com>
2025-09-14 18:56:14 +0530

    Time: 0 ms (100%), Space: 9.2 MB (77.24%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<bool> taken(26,false);
+        vector<int> lastindex(26);
+        string result;
+        for(int i=0;i<s.size();i++){
+            lastindex[s[i]-'a']=i;
+        }
+        for(int i=0;i<s.size();i++){
+            int index=s[i]-'a';
+            if(taken[index]==true) continue;
+            while(result.length()>0&&result.back()>s[i]&&lastindex[result.back()-'a']>i){
+               taken[result.back()-'a']=false;
+                result.pop_back();
+            }
+            result.push_back(s[i]);
+            taken[s[i]-'a']=true;
+        }
+        return result;
+    }
+};
