bcf5858ac2f79442a27f6dd79e5ffd3f986ed596
Abhishek singh <abhisheksinghmd2202@gmail.com>
2025-10-13 22:24:30 +0530

    Time: 6 ms (22.11%), Space: 16.2 MB (54.91%) - LeetHub

---
+++ 1353-find-resultant-array-after-removing-anagrams/1353-find-resultant-array-after-removing-anagrams.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isana(string a,string b){
+        sort(a.begin(),a.end());
+        sort(b.begin(),b.end());
+        return a==b;
+    }
+    vector<string> removeAnagrams(vector<string>& words) {
+        vector<string>ans;
+        ans.push_back(words[0]);
+        for(int i=1;i<words.size();i++){
+            if(isana(words[i],words[i-1])){
+                if(!isana(words[i-1],ans.back())){
+                    ans.push_back(words[i-1]);   
+                }
+            }
+            else ans.push_back(words[i]);
+        }
+        return ans;
+    }
+};
